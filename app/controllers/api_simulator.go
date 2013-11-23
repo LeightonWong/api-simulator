66c012a823ec4e57f7ff86ea3e83c339a2b8635e
Wang Lei <leith.wong@gmail.com>
2013-11-24 00:11:59 +0800

    add random output for api sumulator

--- app/controllers/api_simulator.go
+++ app/controllers/api_simulator.go
@@ -27,12 +27,23 @@ func (as ApiSimulator) Simulate(productId string) revel.Result {
 	var api *models.ProductApi = &models.ProductApi{}
 	err := as.Txn.SelectOne(api, "select * from product_api where product_id = ? and path = ?", productId, parts[1])
 	if err != nil {
-		//panic(err)
-		return as.RenderJson(err)
+		panic(err)
+		//return as.RenderJson(err)
+	}
+	var data models.ApiData
+	err = as.Txn.SelectOne(&data, "select * from api_data where api_id = ? order by rand() limit 0, 1", api.Id)
+	if err != nil {
+		panic(err)
+	}
+	var text string
+	if data.Output != "" {
+		text = data.Output
+	}else{
+		text = api.Output
 	}
 	var output interface{}
-	data := []byte(api.Output)
-	err = json.Unmarshal(data, &output)
+	result := []byte(text)
+	err = json.Unmarshal(result, &output)
 	if err != nil {
 		return as.RenderJson(err)
 	}
